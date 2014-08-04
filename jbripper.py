4f4d9be53f7d4d94f4fa5828485da4913ed90b86
fr34kyn01535 <fr34kyn01535@bam.yt>
2014-08-04 13:12:27 +0200

    Update jbripper.py
    Changed output to be just on one directory (Now customizable).
    Added MP3 validation.

--- jbripper.py
+++ jbripper.py
@@ -9,10 +9,17 @@ import threading
 import time
 
 playback = False # set if you want to listen to the tracks that are currently ripped (start with "padsp ./jbripper.py ..." if using pulse audio)
-
+output_folder = os.getcwd() + "/download/" # change if you want to change output folder
 pipe = None
 ripping = False
+skipping = False
 end_of_track = threading.Event()
+
+def track_name(track):
+    return track.artists()[0].name()+ " - " +track.name()+".mp3" #track.name()+".mp3"
+    
+def folder_name(track):
+    return "/" #track.artists()[0].name() + "/" + track.album().name()
 
 def printstr(str): # print without newline
     sys.stdout.write(str)
@@ -22,37 +29,59 @@ def shell(cmdline): # execute shell commands (unicode support)
     call(cmdline, shell=True)
 
 def rip_init(session, track):
-    global pipe, ripping
+    global pipe, ripping, skipping, output_folder
     num_track = "%02d" % (track.index(),)
-    mp3file = track.name()+".mp3"
-    directory = os.getcwd() + "/" + track.artists()[0].name() + "/" + track.album().name() + "/"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    printstr("ripping " + mp3file + " ...")
-    p = Popen("lame --silent -V2 -h -r - \""+ directory + mp3file+"\"", stdin=PIPE, shell=True)
-    pipe = p.stdin
-    ripping = True
+    mp3file = track_name(track) 
+    directory = output_folder + folder_name(track) + "/"
+    fullpath = directory+mp3file
+    
+    if os.path.isfile(fullpath):
+        if isMp3Valid(fullpath):
+            printstr("skipping " + mp3file + " (already exists)")
+            skipping = True  
+        else:
+            printstr("repeating " + mp3file + " (file corrupt)")
+            skipping = False
+    else:
+        skipping = False
+        
+    if not skipping:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        printstr("ripping " + mp3file + " ...")
+        p = Popen("lame --silent -V2 -h -r - \""+ fullpath +"\"", stdin=PIPE, shell=True)
+        pipe = p.stdin
+        ripping = True
+        f = open("ripping.status",'w')
+        f.write(num_track+"|"+folder_name(track)+"|"+fullpath)
+        f.close()
+    
+	
 
 def rip_terminate(session, track):
     global ripping
     if pipe is not None:
         print(' done!')
         pipe.close()
-    ripping = False    
+    ripping = False
+    f = open("ripping.status",'w')
+    f.write("")
+    f.close()
 
 def rip(session, frames, frame_size, num_frames, sample_type, sample_rate, channels):
     if ripping:
         printstr('.')
         pipe.write(frames);
-
+        
 def rip_id3(session, track): # write ID3 data
+    global output_folder
     num_track = "%02d" % (track.index(),)
-    mp3file = track.name()+".mp3"
+    mp3file = track_name(track)
     artist = track.artists()[0].name()
     album = track.album().name()
     title = track.name()
     year = track.album().year()
-    directory = os.getcwd() + "/" + track.artists()[0].name() + "/" + track.album().name() + "/"
+    directory =  output_folder + folder_name(track) + "/"
 
     # download cover
     image = session.image_create(track.album().cover())
@@ -77,12 +106,145 @@ def rip_id3(session, track): # write ID3 data
     # delete cover
     shell("rm -f cover.jpg")    
 
+def isMp3Valid(file_path):
+    is_valid = False
+
+    f = open(file_path, 'rb')
+    block = f.read(1024)
+    frame_start = block.find(chr(255))
+    block_count = 0 #abort after 64k
+    while len(block)>0 and frame_start == -1 and block_count<64:
+        block = f.read(1024)
+        frame_start = block.find(chr(255))
+        block_count+=1
+        
+    if frame_start > -1:
+        frame_hdr = block[frame_start:frame_start+4]
+        is_valid = frame_hdr[0] == chr(255)
+        
+        mpeg_version = ''
+        layer_desc = ''
+        uses_crc = False
+        bitrate = 0
+        sample_rate = 0
+        padding = False
+        frame_length = 0
+        
+        if is_valid:
+            is_valid = ord(frame_hdr[1]) & 0xe0 == 0xe0 #validate the rest of the frame_sync bits exist
+            
+        if is_valid:
+            if ord(frame_hdr[1]) & 0x18 == 0:
+                mpeg_version = '2.5'
+            elif ord(frame_hdr[1]) & 0x18 == 0x10:
+                mpeg_version = '2'
+            elif ord(frame_hdr[1]) & 0x18 == 0x18:
+                mpeg_version = '1'
+            else:
+                is_valid = False
+            
+        if is_valid:
+            if ord(frame_hdr[1]) & 6 == 2:
+                layer_desc = 'Layer III'
+            elif ord(frame_hdr[1]) & 6 == 4:
+                layer_desc = 'Layer II'
+            elif ord(frame_hdr[1]) & 6 == 6:
+                layer_desc = 'Layer I'
+            else:
+                is_valid = False
+        
+        if is_valid:
+            uses_crc = ord(frame_hdr[1]) & 1 == 0
+            
+            bitrate_chart = [
+                [0,0,0,0,0],
+                [32,32,32,32,8],
+                [64,48,40,48,16],
+                [96,56,48,56,24],
+                [128,64,56,64,32],
+                [160,80,64,80,40],
+                [192,96,80,96,40],
+                [224,112,96,112,56],
+                [256,128,112,128,64],
+                [288,160,128,144,80],
+                [320,192,160,160,96],
+                [352,224,192,176,112],
+                [384,256,224,192,128],
+                [416,320,256,224,144],
+                [448,384,320,256,160]]
+            bitrate_index = ord(frame_hdr[2]) >> 4
+            if bitrate_index==15:
+                is_valid=False
+            else:
+                bitrate_col = 0
+                if mpeg_version == '1':
+                    if layer_desc == 'Layer I':
+                        bitrate_col = 0
+                    elif layer_desc == 'Layer II':
+                        bitrate_col = 1
+                    else:
+                        bitrate_col = 2
+                else:
+                    if layer_desc == 'Layer I':
+                        bitrate_col = 3
+                    else:
+                        bitrate_col = 4
+                bitrate = bitrate_chart[bitrate_index][bitrate_col]
+                is_valid = bitrate > 0
+        
+        if is_valid:
+            sample_rate_chart = [
+                [44100, 22050, 11025],
+                [48000, 24000, 12000],
+                [32000, 16000, 8000]]
+            sample_rate_index = (ord(frame_hdr[2]) & 0xc) >> 2
+            if sample_rate_index != 3:
+                sample_rate_col = 0
+                if mpeg_version == '1':
+                    sample_rate_col = 0
+                elif mpeg_version == '2':
+                    sample_rate_col = 1
+                else:
+                    sample_rate_col = 2
+                sample_rate = sample_rate_chart[sample_rate_index][sample_rate_col]
+            else:
+                is_valid = False
+        
+        if is_valid:
+            padding = ord(frame_hdr[2]) & 2
+            
+            padding_length = 0
+            if layer_desc == 'Layer I':
+                if padding:
+                    padding_length = 4
+                frame_length = (12 * bitrate * 1000 / sample_rate + padding_length) * 4
+            else:
+                if padding:
+                    padding_length = 1
+                frame_length = 144 * bitrate * 1000 / sample_rate + padding_length
+            is_valid = frame_length > 0
+            
+            # Verify the next frame
+            if(frame_start + frame_length < len(block)):
+                is_valid = block[frame_start + frame_length] == chr(255)
+            else:
+                offset = (frame_start + frame_length) - len(block)
+                block = f.read(1024)
+                if len(block) > offset:
+                    is_valid = block[offset] == chr(255)
+                else:
+                    is_valid = False
+        
+    f.close()
+    return is_valid
+
 class RipperThread(threading.Thread):
     def __init__(self, ripper):
         threading.Thread.__init__(self)
         self.ripper = ripper
 
     def run(self):
+        global skipping
         # wait for container
         container_loaded.wait()
         container_loaded.clear()
@@ -103,18 +265,20 @@ class RipperThread(threading.Thread):
         # ripping loop
         session = self.ripper.session
         for track in itrack:
-            
                 self.ripper.load_track(track)
 
                 rip_init(session, track)
 
-                self.ripper.play()
-
-                end_of_track.wait()
-                end_of_track.clear() # TODO check if necessary
-
-                rip_terminate(session, track)
-                rip_id3(session, track)
+                if not skipping:
+                    self.ripper.play()
+                    end_of_track.wait()
+                    end_of_track.clear() # TODO check if necessary
+                    rip_terminate(session, track)
+                    rip_id3(session, track)
+                else:
+                    rip_terminate(session, track)
+                    skipping = False
+                print('\n\n')
 
         self.ripper.disconnect()
 
